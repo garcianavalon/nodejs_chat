90f39123a14aa658ecec30dc26437a509cd92d90
Enrique Garcia Navalon <garcianavalon@gmail.com>
2015-12-03 15:42:34 +0100

    changed template engine to swig

--- app.js
+++ app.js
@@ -8,11 +8,14 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var swig = require('swig');
+
 var app = express();
 
 // view engine setup
+app.engine('html', swig.renderFile);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
